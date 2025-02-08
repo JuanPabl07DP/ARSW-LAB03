d9093b38787edf80c69b3d125159fbe19321a0ec
JuanPabl07DP <juanpadazer15@gmail.com>
2025-02-08 15:38:21 -0500

    Ya se detiene

--- src/main/java/edu/eci/arsw/highlandersim/Immortal.java
+++ src/main/java/edu/eci/arsw/highlandersim/Immortal.java
@@ -2,6 +2,8 @@ package edu.eci.arsw.highlandersim;
 
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Immortal extends Thread {
 
@@ -17,7 +19,6 @@ public class Immortal extends Thread {
 
     private final Random r = new Random(System.currentTimeMillis());
     private boolean execution ;
-    private final Object healthLock = new Object();
 
 
     public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue, ImmortalUpdateReportCallback ucb) {
@@ -32,13 +33,23 @@ public class Immortal extends Thread {
 
     public void run() {
 
-        while (true) {
+        while (this.getHealth() > 0 && immortalsPopulation.size() > 1) {
+            synchronized (immortalsPopulation) {
+                while (!execution) {
+                    try {
+                        immortalsPopulation.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Immortal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
             Immortal im;
 
             int myIndex = immortalsPopulation.indexOf(this);
 
             int nextFighterIndex = r.nextInt(immortalsPopulation.size());
 
+            // avoid self-fight
             if (nextFighterIndex == myIndex) {
                 nextFighterIndex = ((nextFighterIndex + 1) % immortalsPopulation.size());
             }
@@ -46,6 +57,9 @@ public class Immortal extends Thread {
             im = immortalsPopulation.get(nextFighterIndex);
 
             this.fight(im);
+            if (im.getHealth() <= 0) {
+                immortalsPopulation.remove(im);
+            }
 
             try {
                 Thread.sleep(1);
@@ -58,33 +72,40 @@ public class Immortal extends Thread {
     }
 
     public void fight(Immortal i2) {
-        Immortal first = this.name.compareTo(i2.name) < 0 ? this : i2;
-        Immortal second = this.name.compareTo(i2.name) < 0 ? i2 : this;
-
-        synchronized(first.healthLock) {
-            synchronized(second.healthLock) {
-                if (i2.getHealth() > 0) {
-                    i2.changeHealth(i2.getHealth() - defaultDamageValue);
-                    this.health += defaultDamageValue;
-                    updateCallback.processReport("Fight: " + this + " vs " + i2+"\n");
-                } else {
-                    updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+        Immortal firstLock = minImmortalHash(i2);
+        Immortal secondLock = firstLock == this ? i2 : this;
+        synchronized (firstLock) {
+            synchronized (secondLock) {
+                if (this.getHealth() > 0) {
+                    if (i2.getHealth() > 0) {
+                        i2.changeHealth(i2.getHealth() - defaultDamageValue);
+                        this.health += defaultDamageValue;
+                        updateCallback.processReport("Fight: " + this + " vs " + i2 + "\n");
+                    } else {
+                        updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+                    }
                 }
             }
         }
     }
 
-    public void changeHealth(int v) {
-        synchronized(healthLock) {
-            health = v;
+    public Immortal minImmortalHash(Immortal i2) {
+        Immortal minImmortal;
+        int immortalOneHash = System.identityHashCode(this);
+        int immortalTwoHash = System.identityHashCode(i2);
+        if (immortalOneHash < immortalTwoHash) {
+            minImmortal = this;
+        } else if (immortalOneHash > immortalTwoHash) {
+            minImmortal = i2;
+        } else {
+            minImmortal = this;
         }
+        return minImmortal;
     }
 
-    public int getHealth() {
-        synchronized(healthLock) {
-            return health;
-        }
-    }
+    public void changeHealth(int v) {health = v;}
+
+    public int getHealth() {return health;}
 
     public void setExecution(boolean execution) {this.execution = execution;}
 
